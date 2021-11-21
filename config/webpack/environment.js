2880787b3dfa38919f059fc0e25642940f9f2766
Yamaguchi Takuya <yamat47.thirddown@gmail.com>
2021-11-21 16:29:43 +0900

    Give up using webpacker to detect .ts files path.

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,6 +1,4 @@
 const { environment } = require('@rails/webpacker')
-const erb = require('./loaders/erb')
 
-environment.loaders.prepend('erb', erb)
 environment.loaders.delete('nodeModules')
 module.exports = environment
